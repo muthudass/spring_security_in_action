8231c732440f5fb330b93e769d511ebcf3a8dc9a
muthudass <muthudass@yahoo.in>
2020-11-23 15:38:11 +0530

    Project added

---
+++ src/test/java/com/dass/SpringSecurityInAction/SpringSecurityInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dass.SpringSecurityInAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
